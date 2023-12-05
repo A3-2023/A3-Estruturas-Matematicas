a32f5c8f89bce041c84a59ef9b17b762360e6cac
Gabi <gabrielle.sote@gmail.com>
2023-12-05 15:29:48 -0300

    a

--- RepresentacaoGrafica/represetacao_grafica.py
+++ RepresentacaoGrafica/represetacao_grafica.py
@@ -21,7 +21,7 @@ vertices = {
         (-1, -1, -1),
         (0, 1, 0),
     ),
-    'retangle': (  # Substituído "cylinder" por "retangle"
+    'retangle': (
         (0.5, -1, -1),
         (0.5, 1, -1),
         (-0.5, 1, -1),
@@ -47,7 +47,7 @@ edges = {
         (0, 1), (1, 2), (2, 3), (3, 0),
         (0, 4), (1, 4), (2, 4), (3, 4),
     ),
-    'retangle': (  # Substituído "cylinder" por "retangle"
+    'retangle': (
         (0, 1), (1, 2), (2, 3), (3, 0),
         (0, 4), (1, 5), (2, 6), (3, 7),
         (4, 5), (5, 6), (6, 7), (7, 4),
@@ -72,12 +72,10 @@ def main():
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    pygame.display.set_caption("C - Cubo | P - Pirâmide | Y - Retangle")  # Modificado o título da janela
+    pygame.display.set_caption("C - Cubo | P - Pirâmide | Y - Retangle")
 
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
     glTranslatef(0.0, 0.0, -5)
-
-    font = pygame.font.Font(None, 36)
 
     while True:
         for event in pygame.event.get():
@@ -90,7 +88,7 @@ def main():
                 elif event.key == pygame.K_p:
                     change_shape('pyramid')
                 elif event.key == pygame.K_y:
-                    change_shape('retangle')  # Substituído "cylinder" por "retangle"
+                    change_shape('retangle')
 
         glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
