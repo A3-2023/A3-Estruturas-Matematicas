66de412f29ad440525e90385a19e25911e67bbac
Gabi <137116157+gabriellesote@users.noreply.github.com>
2023-12-05 19:43:45 -0300

    Update represetacao_grafica.py

--- RepresentacaoGrafica/represetacao_grafica.py
+++ RepresentacaoGrafica/represetacao_grafica.py
@@ -61,6 +61,8 @@ def draw_shape():
     for edge in edges[current_shape]:
         for vertex in edge:
             glVertex3fv(vertices[current_shape][vertex])
+            # Aqui, vertices[current_shape][vertex] representa um vetor tridimensional que especifica a posição de um vértice no espaço 3D. 
+            # Esses vetores são usados para definir as coordenadas dos vértices dos objetos (cubo, pirâmide, retângulo).
     glEnd()
 
 def change_shape(new_shape):
@@ -75,7 +77,9 @@ def main():
     pygame.display.set_caption("C - Cubo | P - Pirâmide | Y - Retangle")
 
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -5)
+    glTranslatef(0.0, 0.0, -5)   
+    # A função glTranslatef é usada para realizar uma translação no sistema de coordenadas.
+    # Neste caso, ela move a cena para trás ao longo do eixo Z, criando a impressão de que o observador está "afastando-se" dos objetos.
 
     while True:
         for event in pygame.event.get():
@@ -91,6 +95,8 @@ def main():
                     change_shape('retangle')
 
         glRotatef(1, 3, 1, 1)
+        # A função glRotatef é usada para aplicar uma rotação à cena. 
+        # Neste caso, ela realiza uma rotação de 1 grau em torno do eixo (3, 1, 1). Isso cria um efeito de rotação contínua dos objetos na cena.
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         draw_shape()
         pygame.display.flip()
